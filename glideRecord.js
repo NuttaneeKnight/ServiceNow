c60dd6ed96bfed1178e30b11c622fc7dad3c4e35
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-03 08:54:23 -0400

    added the Read method

---
+++ glideRecord.js
@@ -0,0 +1,24 @@
+//READ Method
+
+var incidentGR = new GlideRecord('incident');
+incidentGR.query();
+while(incidentGR.next()) {
+ gs.print(incidentGR.number);
+}
+
+var incidentGR = new GlideRecord("incident");
+incidentGR.query();
+incidentGR.next() //will need this to go into the record
+gs.print(incidentGR.number);
+
+var incidentGR = new GlideRecord('incident');
+incidentGR.addQuery('priority', 1);
+incidentGR.query();
+while(incidentGR.next()) {
+    gs.print('Priority 1 incident: ' + incidentGR.number + ' : ' + incidentGR.priority)
+    // can also Dot Walk to the value Ex. incidentGR.priority.getDisplayValue() -> Will print out the value name display which is critical. 
+}
+
+var incidentGR = new GlideRecord('incident')
+incidentGR.get('ed92e8d173d023002728660c4cf6a7bc')
+gs.print('incidentGR.number' + ' has a sys_id of ' + incidentGR.sys_id)
---
+++ readme.md
@@ -0,0 +1,1 @@
+Notes for the ServiceNow Developer. 
