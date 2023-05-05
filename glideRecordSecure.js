3b121f935a9fc6acadbb65efab6727b1830bca83
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-05 11:39:14 -0400

    GlideRecordSecure

---
+++ glideRecordSecure.js
@@ -0,0 +1,9 @@
+/** 
+ * GlideRecordSecure class inherited from GlideRecord
+ *      Has all the same methods
+ *      Performs ACL checking
+ * Used to secure Script Includes
+ * Replaces canWrite(), canRead(), canUpdate(), canDelete() GlideRecord methods
+ * 
+ * 
+ * */ 
