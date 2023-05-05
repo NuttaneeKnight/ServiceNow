82b9b8d3fbb02a9258043e4c2d3ebfc96ad32837
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-05 15:01:17 -0400

    glideAggregate

---
+++ glideAggregate.js
@@ -0,0 +1,18 @@
+/**
+ * Glide Aggregate
+ * 
+ * Extension of GlideRecord class
+ * Used when performing aggregate queries(count, min, max, sum, avg)
+ *      Reports
+ *      Calculations
+ * Only works on the number fields. 
+ * 
+ */
+
+var count = new GlideAggregate('incident')
+count.addAggregate('COUNT')
+count.query()
+var incident = 0;
+if(count.next()) {
+    incidents = count.getAggregate('COUNT')
+}
