c651607d5bdbb6276015e8a373b27363eaa3580a
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-05 14:56:14 -0400

    sample

--- glideRecordSecure.js
+++ glideRecordSecure.js
@@ -5,5 +5,13 @@
  * Used to secure Script Includes
  * Replaces canWrite(), canRead(), canUpdate(), canDelete() GlideRecord methods
  * 
+ * Will be less lines of code
  * 
  * */ 
+
+
+var incidentGRS = new GlideRecordSecure('incident')
+incidentGRS.query()
+while(incidentGRS.next()) {
+    //action goes here
+}
