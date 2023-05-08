9b1e92ebe4053cedb941a614c02bc01a7a1e7eb0
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-08 10:58:55 -0400

    script ex.

---
+++ glideSystem.js
@@ -0,0 +1,18 @@
+// Print a list of all incidents where the caller is the current user
+
+var incidentGR = new GlideRecord('incident')
+incidentGR.addQuery('caller', gs.getUserID())
+incidentGR.query()
+
+// Create a user greetings
+
+var greetingsMessage = ''
+if(currentHour >= 3 && currentHour < 11) {
+    greetingsMessage = 'Good morning ';
+} else if (currrentHour >= 11 && currentHour < 17) {
+    greetingsMessage = 'Good afternoon ';
+} else {
+    greetingsMessage = 'Good evening ';
+}
+
+greetingsMessage += gs.getUserDisplayName();
