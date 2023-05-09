f42b1c28c8ffd247135252d80377f29f9c24f13f
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-09 11:51:06 -0400

    gs.generateGUID()

--- glideSystem.js
+++ glideSystem.js
@@ -34,4 +34,7 @@ gs.warn('I am a warning message');
 gs.addErrorMessage('Stop! This is an erroe message') // >> Background message, type:error, message: Stop! This is an erroe message
 
 //gs.beginningOfLastMonth()
-gs.print(gs.beginningOfLastMonth())
+gs.print(gs.beginningOfLastMonth()) //Script: 2023-04-01 07:00:00
+
+//gs.generateGUID()
+gs.print(gs.generateGUID())// generate unique global unique identifier => Script: 5c455a4097f2211036743246f053afd0
