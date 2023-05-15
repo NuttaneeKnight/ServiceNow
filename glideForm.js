0f398456fea54585f567a1a0978d674ac23f75f7
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-15 09:21:08 -0400

    code snippet

--- glideForm.js
+++ glideForm.js
@@ -4,3 +4,9 @@
  * setValue() accepts the field name and new value as arguments
  * 
  * */ 
+
+var category = g_form.getValue('category'); //change the category field.
+
+var newCategory = 'software';
+g_forrm.setValue('category', newCategory)
+
