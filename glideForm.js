41550711458c52472165e78b00511463821fe21e
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-15 09:30:50 -0400

    getReference()

--- glideForm.js
+++ glideForm.js
@@ -10,3 +10,14 @@ var category = g_form.getValue('category'); //change the category field.
 var newCategory = 'software';
 g_forrm.setValue('category', newCategory)
 
+/** getReference() method
+ * 
+ * Form only loads fields associated with record n form
+ * use gerReference() to retrieve reference field values
+ * Leverage callbacks
+ * 
+ * */ 
+var caller = g_form.getReference('caller_id', callerCallback)
+function callerCallback(caller) {
+    g_form.setValue('description', caller.first_name + ' ' + caller.last_name + ' says hello.')
+}
