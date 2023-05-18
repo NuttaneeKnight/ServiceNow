7f6d41bf952fdba963c74c6a30e930696e1b56e1
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-18 10:48:54 -0400

    userName()

--- glideUser.js
+++ glideUser.js
@@ -6,5 +6,8 @@ alert('Hello ' + g_user.getFullName())
 // hasRoles() returns boolean
 alert('Do you have any roles? ' + g_user.hasRoles())
 
-//hasRole()
+//hasRole() returns boolean for a specific role
 alert('Do you have ITIL role? ' + g_user.hasRole('itil'))
+
+// userName()
+alert('Your username is: ' + g_user.userName())
