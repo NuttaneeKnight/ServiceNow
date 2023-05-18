09836f81d4ac5d074d8c40ab881842237396ab35
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-18 10:39:48 -0400

    hasRoles()

--- glideUser.js
+++ glideUser.js
@@ -3,3 +3,5 @@ alert('Hello ' + g_user.firstName + ' ' + g_user.lastName + ". Your user ID is: 
 // getFullName()
 alert('Hello ' + g_user.getFullName())
 
+// hasRoles() returns boolean
+alert('Do you have any roles? ' + g_user.hasRoles())
