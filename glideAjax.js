982f149d559e6ed45b2a630ddf5a26078f61cb5f
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-18 11:52:20 -0400

    onLoad()

--- glideAjax.js
+++ glideAjax.js
@@ -1,7 +1,36 @@
-// The GlideAjax process from start to finish
-//Task => Update geolocation on task => Task form
-// 1. Client makes request for a page that contains a Client Script with GlideAjax
-// 2. Server sends client task form data along with Client Script
-// 3. After an onLoad event occurs, the client side script is excuted which calls the GlideAjax API
+/*
+The GlideAjax process from start to finish
+Task => Update geolocation on task => Task form
 
+1. Client makes request for a page that contains a Client Script with GlideAjax
+2. Server sends client task form data along with Client Script
+3. After an onLoad event occurs, the client side script is excuted which calls the GlideAjax API
+4. GlideAjax accesses browser's XMLHttpRequest API and generates a request
+5. Browser's XMLHttpRequest API sends geolocation data back to ServiceNow in the background.
+6. Requestfrom client invokes Script Include, where request data is used to call specific methods with arguments. Then data is packaged upp in the form of a response. 
+7. Browser receives response from the server side
+8. Client Script callback processes returned data and updates location field on task.
 
+*/
+
+// Update the short description field of an incident to Hello world! on load.
+
+//1. Client-side code
+function onLoad() {
+    var ga = new GlideAjax('ServiceNow201GlideAjax')
+    ga.addParam('sysparm_name', 'sayHello')
+    ga.getXML(ajaxProcessor)
+}
+
+function ajaxProcessor(response) {
+    var answer = response.responseXML.documentElement.getAttribute('answer');
+    g_form.setValue('short_description', answer)
+}
+// 2. Server-side code
+var ServiceNow201GlideAjax = Class.create()
+ServiceNow201GlideAjax.prototype = Object.extensObject(AbstractAjaxProcessor, {
+    sayHello: function() {
+        return 'Hello world!'
+    },
+    type: 'ServiceNow201GlideAjax'
+})
