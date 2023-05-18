1546caa6f39e4c1d51f0d98b0409f739ebe8123e
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-18 11:38:03 -0400

    glideAjax process

---
+++ glideAjax.js
@@ -0,0 +1,7 @@
+// The GlideAjax process from start to finish
+//Task => Update geolocation on task => Task form
+// 1. Client makes request for a page that contains a Client Script with GlideAjax
+// 2. Server sends client task form data along with Client Script
+// 3. After an onLoad event occurs, the client side script is excuted which calls the GlideAjax API
+
+
