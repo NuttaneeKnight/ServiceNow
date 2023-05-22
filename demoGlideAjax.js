73151d93b997b0c0f7e1ccf53f210002105e8b11
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-22 15:46:10 -0400

    2nd demo

--- demoGlideAjax.js
+++ demoGlideAjax.js
@@ -29,3 +29,24 @@ Want to log a status of a specific incident every 5s to the UI page.
 
 */
 
+var ServiceNow201GlideAjax = Class.create();
+ServiceNow201GlideAjax.prototype = Object.extendsObject(AbstractAjaxProcessor, {
+
+    getIncidentStatus: function() {
+        var incidentNumber = this.getParameter('sysparm_incident_number');
+        if(!gs.nil(incidentNumber)) {
+            var incidentGR = newGlideRecord('incident');
+            incidentGR.get('number', incidentNumber);
+            return incidentGR.state.getDisplayValue();
+        } else {
+            return 'No incident was found';
+        }
+    },
+
+    sayHello: function() {
+        return 'Hello World!';
+    },
+
+    type: 'ServiceNow201GlideAjax'
+});
+
