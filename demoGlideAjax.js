07a4cc3ddf9d58f96ca05d70032091c5bbc590bc
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-22 09:47:42 -0400

    demo

--- demoGlideAjax.js
+++ demoGlideAjax.js
@@ -2,7 +2,8 @@
 
  grab a string value on script include wait for a call back and update the script include string. 
 
- Go to system admin => script includes and creat a mock script call
+ Go to system admin => script includes and creat a mock script call. 
+ Make sure to check out the "Client Callable box" so that we can call it from the cl
 
 */
 
