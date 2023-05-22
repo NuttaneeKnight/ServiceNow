2680be9adf66f412c8378fb20cd6a6403cf31abb
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-22 10:16:03 -0400

    doc

--- demoGlideAjax.js
+++ demoGlideAjax.js
@@ -1,9 +1,14 @@
 /* Use GlideAjax on an incident form
 
- grab a string value on script include wait for a call back and update the script include string. 
+ Grab a string value on script include wait for a call back and update the script include string. 
 
- Go to system admin => script includes and creat a mock script call. 
- Make sure to check out the "Client Callable box" so that we can call it from the cl
+  - Go to system admin => script includes and creat a mock script call. 
+  - Make sure to check out the "Client Callable box" so that we can call it from the client side
+  - copy the script include name. 
+  - Navigate to the incident table
+  - Click the hanburger icon and navigate to >> Configure (if it's not there, go to the table header names and click right)>> Client Scripts
+  - Create a brand new client script
+
 
 */
 
