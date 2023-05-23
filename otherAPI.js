011197b8a2725d66ec9af1acd404c8ebdde761fd
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-05-23 15:22:15 -0400

    doc

---
+++ otherAPI.js
@@ -0,0 +1,13 @@
+/* 
+GlideDateTime API
+
+The scoped GlideDateTime class provides methods for performing operations on GlideDateTime objects, such as instantiating GlideDateTime objects or working with glide_date_time fields. 
+
+Used for   
+    - Date/time operations
+    - Formatting date/time
+    - Connverting between date//time formats
+
+Server-side API
+
+*/
