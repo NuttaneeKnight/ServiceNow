9fd24dd7b06a589182c9cc0c9c48210c637fd786
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-06-09 15:08:32 -0400

    doc

--- createDog.js
+++ createDog.js
@@ -2,5 +2,8 @@
 jQuery('#addDog').click(function() {
     var ga = new GlideAjax('fetchUtils')
     ga.addParam('sysparm_name', 'createDog')
-    ga.addParam('sysparm_dog_name')
+    ga.addParam('sysparm_dog_name', jQuery('#dogName').val())
+    ga.addParam('sysparm_dog_age', jQuery('#dogAge').val())
+    ga.addParam('sysparm_dog_nutered_or_spay', jQuery('#dogNewteredOrSpay').is(':checked'))
+    ga.addParam('sysparm_dog_shots', jQuery('#dogShots').is(':checked'))
 })
