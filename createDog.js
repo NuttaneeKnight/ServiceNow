5e40ed64614e4dab517fe02768a8c6f1395ea953
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-06-09 15:15:48 -0400

    doc

--- createDog.js
+++ createDog.js
@@ -6,4 +6,10 @@ jQuery('#addDog').click(function() {
     ga.addParam('sysparm_dog_age', jQuery('#dogAge').val())
     ga.addParam('sysparm_dog_nutered_or_spay', jQuery('#dogNewteredOrSpay').is(':checked'))
     ga.addParam('sysparm_dog_shots', jQuery('#dogShots').is(':checked'))
+    ga.getXML(fetchBallback)
 })
+
+function fetchBallback(response) {
+    var answer = response.responseXML.documentElement.getAttribute('answer').split('|');
+    jQuery('#msg').append('<p>' + answer[0] + ' has successfully been created. Dog ID: ' + answer[1] + ' </p><br/><a href="' + answer[2]' + ">View new dog</a>')
+}
