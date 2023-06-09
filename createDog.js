8ca151e8357906b92bb8faa3f99ecfb65be2e9db
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-06-09 15:01:01 -0400

    doc

--- createDog.js
+++ createDog.js
@@ -1,3 +1,6 @@
+//Client Script for the UI Create Dog PAge
 jQuery('#addDog').click(function() {
-    
+    var ga = new GlideAjax('fetchUtils')
+    ga.addParam('sysparm_name', 'createDog')
+    ga.addParam('sysparm_dog_name')
 })
