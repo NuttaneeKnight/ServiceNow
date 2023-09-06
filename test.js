f73f8628b88d85adbdf1a67fe895dd72304c3d03
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-09-06 15:52:26 -0400

    note test fix script

---
+++ test.js
@@ -0,0 +1,31 @@
+var grPRJ = new GlideRecord("pm_project");
+grPRJ.addNotNullQuery("u_planned_start_date");
+grPRJ.query(); // https://lhricdev.service-now.com/now/nav/ui/classic/params/target/pm_project_list.do%3Fsysparm_query%3Du_planned_start_date!%253DNULL%26sysparm_first_row%3D1%26sysparm_view%3D
+grPRJ.setLimit(5);
+while (grPRJ.next()) {
+	grPRJ.setValue("start_date", grPRJ.u_planned_start_date);
+}
+
+var grPRJ2 = new GlideRecord("pm_project");
+grPRJ2.addNotNullQuery("u_planned_end_date");
+grPRJ2.query(); // https://lhricdev.service-now.com/now/nav/ui/classic/params/target/pm_project_list.do%3Fsysparm_query%3Du_planned_end_date!%253DNULL%26sysparm_first_row%3D1%26sysparm_view%3D%26sysparm_choice_query_raw%3D%26sysparm_list_header_search%3Dtrue
+grPRJ2.setLimit(5);
+while (grPRJ2.next()) {
+	grPRJ2.setValue("end_date", grPRJ2.u_planned_end_date);
+}
+
+var grPRJ3 = new GlideRecord("pm_project");
+grPRJ3.addNotNullQuery("u_actual_start_date");
+grPRJ3.query(); // https://lhricdev.service-now.com/now/nav/ui/classic/params/target/pm_project_list.do%3Fsysparm_query%3Du_actual_start_date!%253DNULL%26sysparm_first_row%3D1%26sysparm_view%3D%26sysparm_choice_query_raw%3D%26sysparm_list_header_search%3Dtrue
+grPRJ3.setLimit(5);
+while (grPRJ3.next()) {
+	grPRJ3.setValue("work_start", grPRJ3.u_actual_start_date);
+}
+
+var grPRJ4 = new GlideRecord("pm_project");
+grPRJ4.addNotNullQuery("u_actual_end_date");
+grPRJ4.query(); // https://lhricdev.service-now.com/now/nav/ui/classic/params/target/pm_project_list.do%3Fsysparm_query%3Du_actual_end_date!%253DNULL%26sysparm_first_row%3D1%26sysparm_view%3D%26sysparm_choice_query_raw%3D%26sysparm_list_header_search%3Dtrue
+grPRJ4.setLimit(5);
+while (grPRJ4.next()) {
+	grPRJ4.setValue("work_end", grPRJ4.u_actual_end_date);
+}
