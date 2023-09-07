97f0809fe9d68e1539510289dab1ceda837fe8ad
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-09-07 09:49:23 -0400

    sending inc to another inst

--- triggerRESTToInst2.js
+++ triggerRESTToInst2.js
@@ -1,3 +1,6 @@
 (function execue(current, previous /**null when async */) {
+    var request = new sn_ws.RESTMessageV2("Instance 2 Incident", "create_incident");
+    request.setStringParameterNoEscape("sd", current.short_description);
+    request.setStringParameterNoEscape('desc', current.description);
     
 })
