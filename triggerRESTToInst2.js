802bb9d0c4a8fd05e5123ccdaa03adaa2345e939
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-09-07 09:54:17 -0400

    rest code

--- triggerRESTToInst2.js
+++ triggerRESTToInst2.js
@@ -2,5 +2,13 @@
     var request = new sn_ws.RESTMessageV2("Instance 2 Incident", "create_incident");
     request.setStringParameterNoEscape("sd", current.short_description);
     request.setStringParameterNoEscape('desc', current.description);
-    
-})
+    request.setStringParameterNoEscape("ctype", current.contact_type);
+    var response = request.execute();
+
+    var responseBody = response.getBody();
+    var statusCode = response.getStatusCode();
+
+    var respObj = JSON.parse(responseBody);
+
+    getSelection.log("Response Body - " +"\nStatus Code - " + statusCode, "Incidents Integration Test");
+})(current, previous);
