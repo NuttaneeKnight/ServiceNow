d68c8c26603c4a85eddde9a42a281761b73d5056
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-09-08 08:51:16 -0400

    note

--- triggerRESTToInst2.js
+++ triggerRESTToInst2.js
@@ -1,3 +1,4 @@
+//Business Rules
 (function execue(current, previous /**null when async */) {
     var request = new sn_ws.RESTMessageV2("Instance 2 Incident", "create_incident");
     request.setStringParameterNoEscape("sd", current.short_description);
@@ -10,9 +11,11 @@
 
     var respObj = JSON.parse(responseBody);
     //Do not use current.update inside the business rules. 
-    //current.u_client_ticket_reference = respObj.result.sys_id;
-    //current.update(); !This will make an infinite loop!
+    current.u_client_ticket_reference = respObj.result.sys_id;
+    current.update(); //!This will make an infinite loop!
 
     
     getSelection.log("Response Body - " +"\nStatus Code - " + statusCode, "Incidents Integration Test");
 })(current, previous);
+
+//Script Includes
