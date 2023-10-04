05f68df142a9c2ecd3c959c83c5f8f752dd2de0c
Nuttanee Knight <nuttaneeknight@gmail.com>
2023-10-04 15:29:33 -0400

    date

--- codeSample.js
+++ codeSample.js
@@ -101,7 +101,7 @@
 
 // // <!-- End Custom code to add Record Number, Opened, Short Description and Closure Notes -->
 
-// // Get g_form on console.
+// Get g_form on console.
 // var field = this.angular.element("div[glide-form]").last().scope();
 // if (field && field.hasOwnProperty("$$childHead") && field.$$childHead.hasOwnProperty("getGlideForm")) {
 // 	var my_g_form = field.$$childHead.getGlideForm();
@@ -110,76 +110,207 @@
 // 		//  console.log(my_g_form.getValue("company") );
 // }
 
-// single date
+// // Single date
+// var current = new GlideRecord("sc_req_item");
+
+// if (current.get("be1088b71bad711024a5fd1b1e4bcb84")) {
+//   gs.info(current.getDisplayValue());
+//   gs.info(current.variables.is_this_a_list_of_dates_or_recurring_dates);
+//   gs.info(current.variables.start_date + " " + current.variables.start_time);
+//   createEventTasks(current.variables.start_date, current.variables.start_time);
+// }
+
+// function createEventTasks(date, time) {
+//   var task = new GlideRecord("sc_task");
+
+//   task.initialize();
+//   task.request_item.setValue(current.sys_id);
+//   task.parent.setValue(current.sys_id);
+//   task.cat_item.setValue("32317fbf1b1ee150f3738622dd4bcb78");
+
+//   task.setDisplayValue(
+//     "assignment_group",
+//     current.variables.wf_task_1_assignment_group + ""
+//   );
+//   task.setDisplayValue(
+//     "assigned_to",
+//     current.variables.wf_task_1_assigned_to + ""
+//   );
+//   task.short_description = current.variables.wf_task_1_short_description;
+
+//   var desc = current.variables.wf_task_1_description;
+
+//   if (desc == "") {
+//     task.description =
+//       "Please complete the appropriate steps to fulfill this request: " +
+//       current.variables.wf_task_1_short_description;
+//   } else {
+//     task.description = desc;
+//   }
+
+//   task.description += '\n"Date: "' + date + " time: " + time;
+//   task.insert();
+// }
+
+// // Recurring dates
+// var current = new GlideRecord("sc_req_item");
+
+// if (current.get("d1ffb7a71bad711024a5fd1b1e4bcb31")){
+//     gs.info(current.getDisplayValue());
+//     gs.info(current.variables.is_this_a_list_of_dates_or_recurring_dates);
+//     var eventType = current.variables.is_this_a_list_of_dates_or_recurring_dates
+//     if (eventType == 'Singledate') {
+//         gs.info(current.variables.start_date + " " +  current.variables.start_time);
+//     } else if (eventType == 'Multipledates'){
+
+//     } else {
+//         var dayOfFirstOccurence = current.variables.day_of_first_occurence
+//         var startTime = current.variables.start_time_recurring
+//         var daysOfTheWeek = [
+//             current.variables.monday,
+//             current.variables.tuesday,
+//             current.variables.wednesday, 
+//             current.variables.thursday,  
+//             current.variables.friday,
+//             false,
+//             false,      
+//         ]
+
+//         var numberOfOccurences = current.variables.number_of_occurences
+
+//         var datesArray = [];
+
+//         var gd = new GlideDate()
+//         gd.setValue(dayOfFirstOccurence)
+//         gs.info(gd.getDayOfWeekUTC())
+
+//         var dayOfTheWeek = gd.getDayOfWeekUTC()
+
+//         datesArray.push(gd.getValue());
+
+//         while (datesArray.length <= numberOfOccurences) {
+//             gd.addDays(1)
+//             dayOfTheWeek = gd.getDayOfWeekUTC()
+
+//             if (daysOfTheWeek[dayOfTheWeek-1] == true ) {
+//                 datesArray.push(gd.getValue())
+//             } 
+//             // increment date 
+//             // check for days of the week
+//             // add to array if there's a match
+//             // 
+//         }
+
+
+//         gs.info(datesArray)
+
+//         //for (var day = 0; day < datesArray.length; day++) {
+//             //createEventTasks(datesArray[day],startTime)
+
+//         //}
+
+//     }
+    
+//     //createEventTasks(current.variables.start_date, current.variables.start_time)
+
+// }
+
+// function createEventTasks(date, time) {
+//     var task = new GlideRecord('sc_task')
+    
+//     task.initialize()
+//     task.request_item.setValue(current.sys_id)
+//     task.parent.setValue(current.sys_id)
+//     task.cat_item.setValue('fbff3ba71bad711024a5fd1b1e4bcb58')
+
+//     task.setDisplayValue("assignment_group", current.variables.wf_task_1_assignment_group + '');
+//     task.setDisplayValue("assigned_to", current.variables.wf_task_1_assigned_to + '');
+//     task.short_description = current.variables.wf_task_1_short_description;
+
+//     var desc = current.variables.wf_task_1_description;
+
+//     if(desc == ""){
+// 	    task.description = "Please complete the appropriate steps to fulfill this request: " + current.variables.wf_task_1_short_description ;
+//     } else {
+// 	    task.description = desc;
+//     }
+
+//     task.description += '\n\"Date: "' + date + ' Time: ' + time
+//     task.insert()
+// }
+
+
 var current = new GlideRecord("sc_req_item");
 
-if (current.get("be1088b71bad711024a5fd1b1e4bcb84")) {
-  gs.info(current.getDisplayValue());
-  gs.info(current.variables.is_this_a_list_of_dates_or_recurring_dates);
-  gs.info(current.variables.start_date + " " + current.variables.start_time);
-  createEventTasks(current.variables.start_date, current.variables.start_time);
-}
-
-function createEventTasks(date, time) {
-  var task = new GlideRecord("sc_task");
-
-  task.initialize();
-  task.request_item.setValue(current.sys_id);
-  task.parent.setValue(current.sys_id);
-  task.cat_item.setValue("32317fbf1b1ee150f3738622dd4bcb78");
-
-  task.setDisplayValue(
-    "assignment_group",
-    current.variables.wf_task_1_assignment_group + ""
-  );
-  task.setDisplayValue(
-    "assigned_to",
-    current.variables.wf_task_1_assigned_to + ""
-  );
-  task.short_description = current.variables.wf_task_1_short_description;
-
-  var desc = current.variables.wf_task_1_description;
-
-  if (desc == "") {
-    task.description =
-      "Please complete the appropriate steps to fulfill this request: " +
-      current.variables.wf_task_1_short_description;
-  } else {
-    task.description = desc;
-  }
-
-  task.description += '\n"Date: "' + date + " time: " + time;
-  task.insert();
-}
-
-// recurring dates
-
-var current = new GlideRecord("sc_req_item");
-
-if (current.get("d1ffb7a71bad711024a5fd1b1e4bcb31")){
+if (current.get("61408cb71bad711024a5fd1b1e4bcb1f")){
     gs.info(current.getDisplayValue());
     gs.info(current.variables.is_this_a_list_of_dates_or_recurring_dates);
     var eventType = current.variables.is_this_a_list_of_dates_or_recurring_dates
     if (eventType == 'Singledate') {
+        createEventTasks(current.variables.start_date, current.variables.start_time)
         gs.info(current.variables.start_date + " " +  current.variables.start_time);
     } else if (eventType == 'Multipledates'){
-
+    var mrvsDate = JSON.parse(current.variables.event_dates)
+    gs.info(mrvsDate)
+    for (var day = 0; day < mrvsDate.length; day++) {
+            createEventTasks(mrvsDate[day].what_is_the_date_and_time_of_the_event,mrvsDate[day].start_time_mrvs)
+            gs.info(mrvsDate[day])
+        }
     } else {
         var dayOfFirstOccurence = current.variables.day_of_first_occurence
         var startTime = current.variables.start_time_recurring
-        var daysOfTheWeek = {
-            Monday : current.variables.monday, 
-            Tuesday : current.variables.tuesday,
-        }
+        var daysOfTheWeek = [
+            current.variables.monday,
+            current.variables.tuesday,
+            current.variables.wednesday, 
+            current.variables.thursday,  
+            current.variables.friday,
+            false,
+            false,      
+        ]
 
         var numberOfOccurences = current.variables.number_of_occurences
 
-        gs.info(JSON.stringify(daysOfTheWeek))
-        gs.info(current.variables.monday)
-        gs.info(daysOfTheWeek.Tuesday)
+        var datesArray = [];
+
+        var gd = new GlideDate()
+        gd.setValue(dayOfFirstOccurence)
+        gs.info(gd.getDayOfWeekUTC())
+
+        var dayOfTheWeek = gd.getDayOfWeekUTC()
+
+        datesArray.push(gd.getValue());
+
+        var count = 0
+
+        while (datesArray.length < numberOfOccurences) {
+            gd.addDays(1)
+            gs.info(gd.getValue() + " " + gd.getDayOfWeekUTC() + " " + daysOfTheWeek[dayOfTheWeek-1])
+            dayOfTheWeek = gd.getDayOfWeekUTC()
+
+            if (daysOfTheWeek[dayOfTheWeek-1] == "true" ) {
+                datesArray.push(gd.getValue())
+            } 
+
+
+            // increment date 
+            // check for days of the week
+            // add to array if there's a match
+            // 
+            if (count > 30) break 
+            count++
+
+        }
+
+
+        gs.info(datesArray)
+
+        for (var day = 0; day < datesArray.length; day++) {
+            createEventTasks(datesArray[day],startTime)
+
+        }
+
     }
-    
-    //createEventTasks(current.variables.start_date, current.variables.start_time)
 
 }
 
@@ -189,11 +320,11 @@ function createEventTasks(date, time) {
     task.initialize()
     task.request_item.setValue(current.sys_id)
     task.parent.setValue(current.sys_id)
-    task.cat_item.setValue('32317fbf1b1ee150f3738622dd4bcb78')
+    task.cat_item.setValue('fbff3ba71bad711024a5fd1b1e4bcb58')
 
     task.setDisplayValue("assignment_group", current.variables.wf_task_1_assignment_group + '');
     task.setDisplayValue("assigned_to", current.variables.wf_task_1_assigned_to + '');
-    task.short_description = current.variables.wf_task_1_short_description;
+    task.short_description = current.variables.wf_task_1_short_description + " " + date
 
     var desc = current.variables.wf_task_1_description;
 
@@ -203,7 +334,7 @@ function createEventTasks(date, time) {
 	    task.description = desc;
     }
 
-    task.description += '\n\"Date: "' + date + ' time: ' + time
+    task.description += '\n\Date: ' + date + ' Time: ' + time
     task.insert()
 }
 
